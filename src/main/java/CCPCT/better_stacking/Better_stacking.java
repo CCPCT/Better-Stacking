d43ea222fc84fb1f09f8f1a30a7d934f8ff08409
CC PCT <ccpct.co@gmail.com>
2026-05-31 16:36:04 +0100

    initial: havnt done anything but added stuffs from other mods

---
+++ src/main/java/CCPCT/better_stacking/Better_stacking.java
@@ -0,0 +1,10 @@
+package CCPCT.better_stacking;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Better_stacking implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/CCPCT/better_stacking/client/Better_stackingClient.java
@@ -0,0 +1,10 @@
+package CCPCT.better_stacking.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Better_stackingClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
